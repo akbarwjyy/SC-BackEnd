367feb09ff48105bdb8497fd7928f2071a705ea5
akbarwjyy <dariisayaa@gmail.com>
2023-12-17 00:07:43 +0700

    comentar pada router.use

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const router = express.Router()
-const produkRouter = require('./produk.route')
-const produsenRouter = require('./produsen.route')
+const express = require("express");
+const router = express.Router();
+const produkRouter = require("./produk.route");
+const produsenRouter = require("./produsen.route");
 
-router.use("/produk",produkRouter)
-router.use("/produsen",produsenRouter)
+router.use("/produk", produkRouter);
+router.use("/produsen", produsenRouter);
+// router.use() digunakan untuk memasang middleware untuk rute yang dilayani oleh router tertentu
+// tempat kumpulan dari router-router yang kita buat
 
-
-module.exports = router
+module.exports = router;
