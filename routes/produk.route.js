ce7691811c3cc35d8a72395fbf462520480f85c6
akbarwjyy <dariisayaa@gmail.com>
2023-12-17 00:36:16 +0700

    comentar pada produk.route

--- routes/produk.route.js
+++ routes/produk.route.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
-    produkA,
-    produkB,
-    produkC,
-    produkD,
-    produkE
-} = require('../controllers/produkController')
+  produkA,
+  produkB,
+  produkC,
+  produkD,
+  produkE,
+} = require("../controllers/produkController");
 
-router.get("/",produkA)
-router.get("/:id",produkB)
-router.post('/', produkC)
-router.delete('/:id', produkD)
-router.patch('/:id', produkE)
+router.get("/", produkA);
+router.get("/:id", produkB);
+router.post("/", produkC);
+router.delete("/:id", produkD);
+router.patch("/:id", produkE);
 
-module.exports = router
+// router.get untuk meminta data dari produkA dan id produkB. Dan meminta router.post untuk,
+// router.post untuk mengirimkan data ke produkC untuk diproses.
+// router.dalete untuk menghapus data
+// router.patch digunakan untuk melakukan hanya sebagai resource ke sebuah server.
+
+module.exports = router;
