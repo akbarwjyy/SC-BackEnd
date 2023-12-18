fc21ec5eb1c79fc35b45a351822e28d3f7cf1ddf
akbarwjyy <dariisayaa@gmail.com>
2023-12-18 19:33:48 +0700

    comentar pada app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const Route = require('./routes');
+const express = require("express");
+const app = express();
+const port = 3000;
+const Route = require("./routes");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(Route)
+app.use(Route);
 
-app.listen (port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+/**server HTTP dengan Express, menggunakan middleware untuk memproses JSON,
+ * dan memiliki rute-rute yang dikelola oleh modul "routes". Server mendengarkan
+ * pada port 3000 dan mencetak pesan ke konsol ketika berhasil berjalan.
+ */
